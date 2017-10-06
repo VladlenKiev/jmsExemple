926a18a4fa7b093357645d03f0f76b72c61824d8
vladlenkiev <mankovskij@yandex.ru>
2017-10-06 17:06:35 +0300

    To TestBranch1

---
+++ src/main/java/com/company/activemq/reciever/TestStatOne.java
@@ -0,0 +1,19 @@
+package com.company.activemq.reciever;
+
+/**
+ * Created by admin on 31.08.2017.
+ */
+public class TestStatOne {
+
+    static Integer aInt;
+    static {
+        int pInt;
+    }
+
+    Integer nonStat;
+
+    public TestStatOne(){
+        aInt =0;
+    }
+
+}
