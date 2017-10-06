becf17cab6e4c5f38741b5c5fb54e3e67778d57a
Volodymyr <mankovskij@yandex.ru>
2017-10-06 23:45:52 +0300

    web

---
+++ src/main/java/com/company/activemq/reciever/web/WebTestStat.java
@@ -0,0 +1,19 @@
+package com.company.activemq.reciever.web;
+
+/**
+ * Created by admin on 31.08.2017.
+ */
+public class WebTestStat {
+
+    static Integer aInt;
+    static {
+        int pInt;
+    }
+
+    Integer nonStat;
+
+    public WebTestStat(){
+        aInt =0;
+    }
+
+}
